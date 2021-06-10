914b492111b3cade6e6487c6c47d46c62f418440
Arnab Roy Chowdhury <arnab.roy.chowdhury.developer@gmail.com>
2021-06-10 07:42:11 +0530

    wip

--- ArnabDeveloper.HttpHealthCheck.DI/ServiceCollectionExtensions.cs
+++ ArnabDeveloper.HttpHealthCheck.DI/ServiceCollectionExtensions.cs
@@ -2,8 +2,16 @@
 
 namespace ArnabDeveloper.HttpHealthCheck.DI
 {
+    /// <summary>
+    /// Service collection extensions
+    /// </summary>
     public static class ServiceCollectionExtensions
     {
+        /// <summary>
+        /// Add http health check
+        /// </summary>
+        /// <param name="services">IServiceCollection type object</param>
+        /// <returns>IServiceCollection type object</returns>
         public static IServiceCollection AddHttpHealthCheck(this IServiceCollection services) =>
             services
                 .AddHttpClient()
